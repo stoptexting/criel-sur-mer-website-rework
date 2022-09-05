6aa41c1a219e76f57f9657e4c8ad74f7ecb9a226
stopsimping <gaspard.chevalier@etu.u-paris.fr>
2022-09-05 22:07:02 +0200

    typedjs implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-$(window).on('scroll', function () {
-  var pixs = $(document).scrollTop()
-  pixs = pixs / 10;
-  $(".out").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" })     
-});
-
 $( document ).ready(function() {
   $('body').css("overflow", "hidden");
 });
