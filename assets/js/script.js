0be2fdd9b1bd7f440a8b4ca890d56534f4679111
stopsimping <gaspard.chevalier@etu.u-paris.fr>
2022-09-05 22:18:49 +0200

    typed update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
   $('body').css("overflow", "hidden");
+  const video = document.getElementById('video-background');
+  video.currentTime += 5;
 });
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navigando'
+})
+
