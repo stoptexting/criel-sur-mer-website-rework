ddeec10cb424f7d30b461ae6bd13751d82108d2e
stopsimping <gaspard.chevalier@etu.u-paris.fr>
2022-09-06 13:06:43 +0200

    big update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,33 @@ $( window ).on( "load", function() {
   video.currentTime += 6;
 });
 
+function CheckSlugValidity(slug){
+  return slug.indexOf(window.location.pathname) == 0;
+}
+
 $( document ).ready(function() {
   $('body').css("overflow", "hidden");
 
   
-  const main = document.getElementById('main_page');
-  main.style.display = "none";
-
+  if ((CheckSlugValidity("/") || CheckSlugValidity("/index.html"))) {
+    const main = document.getElementById('main_page');
+    main.style.display = "none";
   
+    
+  
+    var typed = new Typed('#typed',{
+      strings:["Bienvenue.","Criel-sur-Mer vous attend !", "Qu'attendez vous?"],
+      backSpeed: 40,
+      typeSpeed: 40
+    });
+  }
 
-  var typed = new Typed('#typed',{
-    strings:["Bienvenue.","Criel-sur-Mer vous attend !", "Qu'attendez vous?"],
-    backSpeed: 40,
-    typeSpeed: 40
-  });
+  else {
+    const main = document.getElementById('main_page');
+    main.style.display = "block";
+    main.style.visibility = "visible";
+  }
+  
 });
 
 function init() {
