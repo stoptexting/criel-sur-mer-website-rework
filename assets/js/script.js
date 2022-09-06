4581ef3986cb578ab9aad890911a8c2aebd90d33
stopsimping <gaspard.chevalier@etu.u-paris.fr>
2022-09-06 21:13:57 +0200

    liver server fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,21 @@ $( window ).on( "load", function() {
 });
 
 function CheckSlugValidity(slug){
-  return slug.indexOf(window.location.pathname) == 0;
+  console.log("mdr");
+  return window.location.pathname.includes(slug);
 }
+
+//   return slug.indexOf(window.location.pathname) == 0; pour live server
 
 $( document ).ready(function() {
   $('body').css("overflow", "hidden");
-
+  console.log("ptdr");
   
   if ((CheckSlugValidity("/") || CheckSlugValidity("/index.html"))) {
+    console.log("mdr");
     const main = document.getElementById('main_page');
     main.style.display = "none";
+    
   
     var typed = new Typed('#typed',{
       strings:["Bienvenue.","Criel-sur-Mer vous attend !", "Qu'attendez vous?"],
