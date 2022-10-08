efdb1b3be8477ecdaa365a58daaa01fc4db6b2fa
stopsimping <87366457+stopsimping@users.noreply.github.com>
2022-10-08 07:16:49 +0200

    dsfdfs

--- www/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./assets/**/*.{html,js}", "./*.{html,js}", './node_modules/tw-elements/dist/js/**/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('tw-elements/dist/plugin')],
-}
